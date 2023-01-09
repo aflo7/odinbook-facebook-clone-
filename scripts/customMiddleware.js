e67cd4e27f97b6e094e8f179f236ed5378a39c96
Andres Flores <andresflo883@gmail.com>
2023-01-08 23:48:47 -0500

    edited gitignore

---
+++ scripts/customMiddleware.js
@@ -0,0 +1,28 @@
+var { User, Post, Comment } = require("../models/schema")
+
+function isAuthenticated(req, res, next) {
+  if (req.user) return next()
+  else return res.redirect("/")
+}
+
+function getUsersNotFollowing(req, res, next) {
+  const currentUserId = req.user._id
+  // retrieve all users except the current user
+  User.find(
+    { _id: { $ne: currentUserId } },
+    "name _id",
+    function (err, allUsers) {
+      if (err) {
+        return res.sendStatus(400)
+      }
+      const usersFollowing = req.user.following
+      const usersNotFollowing = allUsers.filter(
+        (user) => !usersFollowing.includes(user._id)
+      )
+      res.locals.allUsers = usersNotFollowing
+      next()
+    }
+  )
+}
+
+module.exports = { getUsersNotFollowing, isAuthenticated }
