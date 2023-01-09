df45d74b1823f1349f83ab0d4a6a774e080d7fca
Andres Flores <andresflo883@gmail.com>
2023-01-09 01:00:25 -0500

    .

--- routes/image.js
+++ routes/image.js
@@ -20,7 +20,9 @@ function clearUploadsFolder() {
 }
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads")
+    // cb(null, "uploads")
+    cb(null, path.join(__dirname, "..", 'uploads/'))
+    
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + "-" + Date.now())
