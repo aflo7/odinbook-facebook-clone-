7c1eeabeb2a714a577da92ac69eb24fb8c30e693
Andres Flores <andresflo883@gmail.com>
2023-05-09 08:36:09 -0400

    add unfollow btn and unfollow functionality

--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,80 @@
 var express = require("express")
 var router = express.Router()
 var {
-  isAuthenticated,
-  getUsersNotFollowing
+    isAuthenticated,
+    getUsersNotFollowing
 } = require("../scripts/customMiddleware.js")
 var { User } = require("../models/schema.js")
 
 router.post("/follow", isAuthenticated, (req, res) => {
-  User.findById(req.user._id, (err, foundUser) => {
-    if (err) return res.render("error")
+    User.findById(req.user._id, (err, foundUser) => {
+        if (err) return res.render("error")
 
-    foundUser.following.push(req.body.userIdToFollow)
-    foundUser.save(function (err, result) {
-      if (err) {
-        return res.render('error')
-      } else {
-        return res.redirect('/users/find-friends')
-      }
+        foundUser.following.push(req.body.userIdToFollow)
+        foundUser.save(function (err, result) {
+            if (err) {
+                return res.render("error")
+            } else {
+                return res.redirect("/users/find-friends")
+            }
+        })
     })
-  })
+})
+
+router.post("/unfollow", isAuthenticated, (req, res) => {
+    User.findById(req.user._id, (err, foundUser) => {
+        if (err) return res.render("error")
+        const filteredArr = foundUser.following.filter(
+            (user) => user._id.toString() !== req.body.userIDToUnfollow
+        )
+        foundUser.following = filteredArr
+        foundUser.save(function (err, result) {
+            if (err) {
+                return res.render("error")
+            } else {
+                return res.redirect("/users/find-friends")
+            }
+        })
+    })
 })
 
 router.post("/toggle-dark-mode", isAuthenticated, (req, res) => {
-  const userId = req.user._id
-  User.findById(userId, (err, foundUser) => {
-    if (err) return res.render("error")
+    const userId = req.user._id
+    User.findById(userId, (err, foundUser) => {
+        if (err) return res.render("error")
 
-    foundUser.settings.darkMode = !foundUser.settings.darkMode
-    foundUser.save((err, result) => {
-      if (err) return res.render("error")
+        foundUser.settings.darkMode = !foundUser.settings.darkMode
+        foundUser.save((err, result) => {
+            if (err) return res.render("error")
 
-      return res.redirect("/profile")
+            return res.redirect("/profile")
+        })
     })
-  })
 })
 
 router.get(
-  "/find-friends",
-  isAuthenticated,
-  getUsersNotFollowing,
-  (req, res, next) => {
-    // get a list of users currently following
-    User.findById(req.user._id)
-      .populate("following")
-      .exec((err, foundUser) => {
-        if (err) return res.redirect("/")
+    "/find-friends",
+    isAuthenticated,
+    getUsersNotFollowing,
+    (req, res, next) => {
+        // get a list of users currently following
+        User.findById(req.user._id)
+            .populate("following")
+            .exec((err, foundUser) => {
+                if (err) return res.redirect("/")
 
-        if (!foundUser.following || foundUser.following.length === 0) {
-          res.locals.currentFriendNames = []
-          return res.render("find-friends")
-        }
+                if (!foundUser.following || foundUser.following.length === 0) {
+                    res.locals.currentFriendNames = []
+                    return res.render("find-friends")
+                }
 
-        const namesOfPeopleYouAreFollowing = foundUser.following.map(
-          (person) => person.name
-        )
-        res.locals.currentFriendNames = namesOfPeopleYouAreFollowing
-        return res.render("find-friends")
-      })
-  }
+                const namesOfPeopleYouAreFollowing = foundUser.following.map(
+                    (person) => person
+                )
+                res.locals.currentFriendNames = namesOfPeopleYouAreFollowing
+                return res.render("find-friends")
+            })
+    }
 )
 
 module.exports = router
