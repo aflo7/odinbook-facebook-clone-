7bdadb58f173ce633bff861c087c0335c6344f31
Andres Flores <andresflo883@gmail.com>
2023-10-20 15:14:21 -0400

    cleaned code for the shuffly function

--- scripts/shuffle.js
+++ scripts/shuffle.js
@@ -1,15 +1,15 @@
 function shuffle(array) {
-    const newArray = [...array]
+  const newArray = [...array];
 
-    for (let start = 0; start < newArray.length; start++) {
-        const randomPosition = Math.floor(
-            (newArray.length - start) * Math.random()
-        )
-        const randomItem = newArray.splice(randomPosition, 1)
-        newArray.push(...randomItem)
-    }
+  for (let start = 0; start < newArray.length; start++) {
+    const randomPosition = Math.floor(
+      (newArray.length - start) * Math.random()
+    );
+    const randomItem = newArray.splice(randomPosition, 1);
+    newArray.push(...randomItem);
+  }
 
-    return newArray
+  return newArray;
 }
 
-module.exports = shuffle
+module.exports = shuffle;
