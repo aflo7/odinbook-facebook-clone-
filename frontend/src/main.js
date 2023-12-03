7ff3934952b4632f5627c2dc20cb0b92c97254d5
Andres Flores <andresflo883@gmail.com>
2023-12-03 12:20:47 -0500

    added login route (not complete)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from "vue-router"
 
-createApp(App).mount('#app')
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+const app = createApp({})
+app.use(router)
+app.mount('#app')
