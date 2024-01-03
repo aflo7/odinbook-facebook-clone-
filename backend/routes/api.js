c158a3d1e29ec27681d8a312bcac21830bfb7807
Andres Flores <andresflo883@gmail.com>
2024-01-03 05:47:41 -0500

    updated readmes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,33 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { appleArticles, chatGptArticles } = require('../server/newsData');
 var jwt = require('jsonwebtoken');
-// everything here starts with /api
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the API'
-  });
-});
-
-router.get('/articles', (req, res) => {
-  res.json({
-    appleArticles,
-    chatGptArticles
-  });
-});
-
-router.post('/login', (req, res) => {
-  if (req.body.username === 'guest' && req.body.password === 'pass') {
-    const data = {
-      time: Date(),
-      userId: 12
-    };
-
-    const token = jwt.sign(data, process.env.JWTSECRETKEY);
-
-    return res.json({ token });
-  }
-  res.sendStatus(401);
-});
+var { User, Post } = require('../models/schema.js');
 
 const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization');
@@ -47,8 +21,60 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  });
+});
+
+router.get('/articles', (req, res) => {
+  res.json({
+    appleArticles,
+    chatGptArticles
+  });
+});
+
+router.post('/home', (req, res) => {
+  var currentUserId = jwt.verify(req.body.token, process.env.JWTSECRETKEY);
+  User.findById(currentUserId, (err, result) => {
+    if (err) return res.render('error');
+    const userIdsToFind = result.following;
+    userIdsToFind.push(result._id);
+
+    Post.find({
+      posterId: {
+        $in: userIdsToFind
+      }
+    })
+      .sort({ _id: -1 })
+      .exec((err, result) => {
+        if (err) return res.json({ message: 'error' });
+        return res.json({ posts: result });
+      });
+  });
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (err) {
+      return res.json({ message: 'error' });
+    }
+
+    if (foundUser) {
+      if (req.body.password == foundUser.password) {
+        const foundUserId = foundUser._id.toJSON();
+        const token = jwt.sign(foundUserId, process.env.JWTSECRETKEY);
+        return res.json({ token });
+      }
+      return res.json({ message: 'Incorrect password' });
+    }
+
+    return res.json({ message: 'Incorrect username' });
+  });
+});
+
 router.get('/protected-resource', authenticateToken, (req, res) => {
-  res.json({ message: 'here is the protected resources' });
+  return res.json({ message: 'here is the protected resources' });
 });
 
 module.exports = router;
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,60 @@
 <script setup>
 import axios from 'axios';
 import './css/styles.css';
+import './css/App.css';
 </script>
 
 <template>
-  <form @submit.prevent="submit()">
+  <div v-if="loggedIn">
+    <nav>
+      <div style="display: flex; align-items: center; gap: 10px">
+        <img src="./assets/odinlogo2.jpeg" height="40px" />
+        <div>Odinbook</div>
+      </div>
+      <form @submit.prevent="logout()">
+        <button type="submit">Logout</button>
+      </form>
+    </nav>
+    <div class="main-wrapper">
+      <main>
+        <div class="mindWrapper">
+          <div class="mindWrapperTop">
+            <div>PFP</div>
+            <input
+              class="mindTextBox"
+              type="text"
+              placeholder="What's on your mind, Andres?"
+            />
+          </div>
+          <hr />
+          <div class="mindWrapperBottom">
+            <div>Live video</div>
+            <div>Photo/video</div>
+            <div>Feeling/activity</div>
+          </div>
+        </div>
+
+        <div v-if="posts && posts.length > 0">
+          <div v-for="(post, index) in posts" :key="index" class="post">
+            <div>
+              {{ post.content }}
+            </div>
+            <div>
+              {{ post.posterName }}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <form
+    v-else
+    @submit.prevent="submit"
+    style="display: flex; flex-direction: column; gap: 10px; margin: 20px;"
+  >
     <input type="text" placeholder="Username" v-model="username" />
     <input type="text" placeholder="Password" v-model="password" />
     <button type="submit">Login</button>
-  </form>
-
-  <form @submit.prevent="getProtectedResource()" style="border: 2px solid red">
-    <button type="submit">Get the protected resource</button>
-  </form>
-
-  <form @submit.prevent="logout()">
-    <button type="submit">Logout</button>
   </form>
 </template>
 
@@ -24,7 +63,9 @@ export default {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      loggedIn: false,
+      posts: null
     };
   },
   methods: {
@@ -34,12 +75,18 @@ export default {
           username: this.username,
           password: this.password
         })
-        .then(function (response) {
+        .then((response) => {
+          this.username = ""
+          this.password = ""
+          this.loggedIn = true;
           localStorage.setItem('token', response.data.token);
+
+          this.loadHomePage();
         })
-        .catch(function (error) {
+        .catch((error) => {
+          this.username = ""
+          this.password = ""
           console.log(error);
-          alert('incorrect credentials');
         });
     },
     getProtectedResource() {
@@ -49,18 +96,41 @@ export default {
             Authorization: localStorage.getItem('token')
           }
         })
-        .then(function (response) {
+        .then((response) => {
           console.log(response.data);
         })
         .catch((error) => {
+          this.loggedIn = false;
           localStorage.removeItem('token');
         });
     },
     logout() {
+      this.loggedIn = false;
       localStorage.removeItem('token');
+    },
+    loadHomePage() {
+      axios
+        .post(
+          'http://localhost:4000/api/home',
+          {
+            token: localStorage.getItem('token')
+          },
+          {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            }
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.posts = response.data.posts;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }
 };
 </script>
-<style scoped>
-</style>
+
+<style scoped></style>
